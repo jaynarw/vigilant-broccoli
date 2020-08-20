bc9d1546b8e0ed8d656b23b4133ea2e9004c3296
jaynarw <jnarwaria@gmail.com>
2020-08-20 07:51:25 +0530

    Fix fetch for async funcitons, Add dynamic progress

--- articleFetcher.js
+++ articleFetcher.js
@@ -8,6 +8,18 @@ const { JSDOM } = jsdom;
 
 const articleData = {};
 
+const logUpdate = require('log-update');
+
+const frames = ['-', '\\', '|', '/'];
+let i = 0;
+let percentage = 0;
+const id = setInterval(() => {
+	const frame = frames[i = ++i % frames.length];
+	logUpdate(
+`♥ Running: ${Math.round((percentage + Number.EPSILON) * 100) / 100}% complete... ${frame} ♥`
+	);
+}, 80);
+
 (async () => {
   
   const articlesJSON = await readFile('Articles.json');
@@ -15,30 +27,37 @@ const articleData = {};
   let articles = JSON.parse(articlesJSON);
   articles = { '44062' : articles['44062'] };
   
-  await Promise.all(
-    Object.keys(articles).map(async (key) => {
-      const articleTitles = Object.keys(articles[key]);
-      articleData[key] = {};
-      await Promise.all(
-        articleTitles.map((async (title) => {
-          const url = articles[key][title];
-          const response = await fetch(url);
-          if(response.status === 200) {
-            const html = await response.text();
-            const { document } = (new JSDOM(html)).window;
-            const articleMetadata = document.evaluate("/html/body//script[@type='application/ld+json'][1]", document, null, 7, null);
-            if (articleMetadata.snapshotLength > 0) {
-              articleData[key][title] = JSON.parse(articleMetadata.snapshotItem(0).textContent);
-            }
-          }
-        }))
-      );
-    })
-  );
-  fs.writeFile('ArticlesData.json', JSON.stringify(articleData, null, 2), (err) => {
+  const days = Object.keys(articles);
+
+  for(let ind = 0; ind < days.length; ind++ ) {
+    const articleTitles = Object.keys(articles[days[ind]]);
+    articleData[days[ind]] = {};
+
+    for(let articleInd = 0; articleInd < articleTitles.length; articleInd++) {
+      percentage = ind/days.length + (articleInd / (articleTitles.length)) * (100/days.length);
+      // console.log(percentage);
+      const url = articles[days[ind]][articleTitles[articleInd]];
+      const response = await fetch(url);
+      if(response.status === 200) {
+        const html = await response.text();
+        const virtualConsole = new jsdom.VirtualConsole();
+        virtualConsole.sendTo(console, { omitJSDOMErrors: true });
+        const { document } = (new JSDOM(html, {
+          contentType: "text/html",
+          virtualConsole
+        })).window;
+        const articleMetadata = document.evaluate("/html/body//script[@type='application/ld+json'][1]", document, null, 7, null);
+        if (articleMetadata.snapshotLength > 0) {
+          articleData[days[ind]][articleTitles[articleInd]] = JSON.parse(articleMetadata.snapshotItem(0).textContent);
+        }
+      }
+    }
+  }
+  fs.writeFile('ArticlesData.json', JSON.stringify(articleData), (err) => {
     if(err) throw err;
     else {
-      console.log('MwaaahH!!!');
+      clearInterval(id);
+      logUpdate('MwaaahH!!! ♥');
     }
   });
 })();
